25d388b377cdbc95c583c44d80a20741a1cd9fbb
Mudassar Ali <alimudassar111@live.com>
2017-12-18 23:31:35 +0500

    Starter Kit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const theme = createMuiTheme();
